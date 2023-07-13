b84aef3d2c9cb4a731b30e7161dc5ac4059f948c
kushal752 <kushaldabbe752@gmail.com>
2023-07-13 23:06:52 +0530

    feed component

---
+++ app/api/prompt/route.js
@@ -0,0 +1,10 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = (req) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
